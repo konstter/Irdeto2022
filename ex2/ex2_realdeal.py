f15859eac509fdac050aac238f7de668e61825a0
konstter <konst.br@gmail.com>
2022-07-02 08:34:31 +0800

    add changes to ex2

--- ex2/ex2_realdeal.py
+++ ex2/ex2_realdeal.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
+
 import os
+import argparse
 
 
 def read_files(d):
     result_dict = {}
     q = 0
-    for fn in os.listdir("test_algorithm"):
-        with open(os.path.join("test_algorithm", fn), 'r') as f:
+    for fn in os.listdir(d):
+        with open(os.path.join(d, fn), 'r') as f:
 
             for str in f:
                 str_list = str.split(",", 1)
@@ -27,11 +30,48 @@ def analyze(d, pc):
         part = (r_dict[key] / q) * 100
         if part >= pc:
             r_list.append(key)
-    return r_list
+    output(r_list)
+
+
+def output(r_l):
+    r_l.sort()
+    lenght = len(r_l)
+    with open('output.txt', 'w') as f:
+        for i in range(lenght):
+            if i == lenght - 1:
+                f.write(str(r_l[i]))
+            else:
+                f.write(str(r_l[i])+',')
+
+
+def create_parser():
+    '''
+    Start bash parser
+    '''
+    parser = argparse.ArgumentParser()
+    h1_mes = 'A directory to analyze files'
+    h2_mes = 'A percentage'
+    parser.add_argument('-d', help=h1_mes)
+    parser.add_argument('-p', help=h2_mes)
+    return parser
+
+
+def driver():
+    '''
+    Initial with default or user options
+    '''
+    args = create_parser().parse_args()
+    directory = 'test_algorithm'
+    percentage = 50
+
+    if args.d:
+        directory = args.d
+    if args.p:
+        percentage = args.p
+
+    analyze(directory, float(percentage))
 
 
 if __name__ == '__main__':
 
-    dir = "test_algorithm"
-    p = 10
-    print(analyze(dir, float(p)))
+    driver()
