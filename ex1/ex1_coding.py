3d712c60f12f91d628aba7aa8145e0c874189496
konstter <konst.br@gmail.com>
2022-07-06 13:07:51 +0800

    add return to def solution

--- ex1/ex1_coding.py
+++ ex1/ex1_coding.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python
 
 def solution(A):
-    global c
-    s_set, c1 = [], 0
-    for n in A:
-        if n:
-            if n & 1:
-                c1 += 1
-            s_set.append(n >> 1)
-    if c1 > c:
-        c = c1
-    if c < len(s_set):
-        solution(s_set)
+
+    def f(Ar, c):
+        s_set, c1 = [], 0
+        for n in Ar:
+            if n:
+                if n & 1:
+                    c1 += 1
+                s_set.append(n >> 1)
+        if c1 > c:
+            c = c1
+        if c < len(s_set):
+            return f(s_set, c)
+        else:
+            return c
+
+    return f(A, 0)
 
 
-c = 0
 # a_array = Array([0])
-# a_array = Array([13, 7, 3])
+# a_array = Array([1, 2, 4, 8])
 # a_array = Array([2, 2])
-a_array = [1, 2, 4, 8]
-solution(a_array)
-print(c)
+a_array = [13, 7, 3, 1]
+print(solution(a_array))
